de8a46ff9ae5efa0cb8bf192926367d6d9e17ffc
Junwoo Lee <56960550+joonrb@users.noreply.github.com>
2024-04-23 21:31:50 +0400

    YUMI WILL PULL THIS

---
+++ goodNight/src/firebase/firebaseConfig.js
@@ -0,0 +1,18 @@
+import firebase from '@react-native-firebase/app';
+import firestore from '@react-native-firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDat4mEjt-U4cRBLq-6yKr7hew7dugwCU8",
+  authDomain: "goodnight-23bd4.firebaseapp.com",
+  databaseURL: "https://goodnight-23bd4-default-rtdb.europe-west1.firebasedatabase.app/",
+  projectId: "goodnight-23bd4",
+  storageBucket: "goodnight-23bd4.appspot.com",
+  messagingSenderId: "506276774022",
+  appId: "1:506276774022:android:229cf128b2a06b15b65107"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
