a876fb6b80eb3993712f114bcc3b71cdb4a703dc
Druce Chua <89497828+druceC@users.noreply.github.com>
2024-04-30 20:43:18 +0400

    Update Questionnaire.js

--- goodNight/src/screens/Questionnaire.js
+++ goodNight/src/screens/Questionnaire.js
@@ -5,16 +5,16 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Welcome from "./Welcome";
 import QuizPage from "./QuizPage";
 import Result from "./Result";
-// import firestore from '@react-native-firebase/firestore';
+import firestore from '@react-native-firebase/firestore';
 import { Audio } from 'expo-av';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import Slider from '@react-native-community/slider';
 // import firebase from 'firebase';
 
 const Stack = createNativeStackNavigator();
-export default function CBTIQuestionnairePage_v1() {
+export default function CBTIQ() {
   return (
-    <NavigationContainer>
+    <NavigationContainer independent={true}>
       <Stack.Navigator initialRouteName="Welcome">
         <Stack.Screen
           name="Welcome"
