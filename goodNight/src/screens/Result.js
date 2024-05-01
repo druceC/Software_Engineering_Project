96a3fd5947ebdc3bcc95aee08c5fafbea9f53c65
Druce Chua <89497828+druceC@users.noreply.github.com>
2024-05-01 20:35:57 +0400

    Update Result.js

--- goodNight/src/screens/Result.js
+++ goodNight/src/screens/Result.js
@@ -1,21 +1,42 @@
+import React, { useEffect } from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
 
 const Result = ({ navigation, route }) => {
-  const { score } = route.params;
-  return (
-    <View style={styles.container}>
-      <View style={styles.subContainer}>
-        <Text style={{ fontSize: 50 }}>Your Sleep Score</Text>
+    const { score } = route.params;
+    const uid = auth().currentUser.uid;
+    // const uid = await firestore().collection('usernames').get();
 
-        <View style={styles.textWrapper}>
-          <Text style={styles.score}>{score}</Text>
-          <Text style={styles.score}> / 28</Text>
-        </View>
-        {/* Retry Quiz button */}
-        <TouchableOpacity
-          onPress={() => {
-            navigation.navigate("Welcome");
+    const saveSleepScoreToFirestore = async (score, uid) => {
+        try {
+            await firestore().collection('sleepScore').add({
+                score: score,
+                uid: uid,
+                timestamp: firestore.FieldValue.serverTimestamp()
+            });
+            console.log("Sleep score saved successfully!");
+        } catch (error) {
+          console.error("Error saving sleep score: ", error);
+        }
+    };
+    useEffect(() => {
+        saveSleepScoreToFirestore(score, uid);
+    }, []);
+
+    return (
+        <View style={styles.container}>
+        <View style={styles.subContainer}>
+            <Text style={{ fontSize: 50 }}>Your Sleep Score</Text>
+
+            <View style={styles.textWrapper}>
+            <Text style={styles.score}>{score}</Text>
+            <Text style={styles.score}> / 28</Text>
+            </View>
+            {/* Retry Quiz button */}
+            <TouchableOpacity
+            onPress={() => {
+                navigation.navigate("Welcome");
           }}
           style={styles.btnReset}
         >
