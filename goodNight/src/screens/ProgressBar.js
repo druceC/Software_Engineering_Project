909b83c3093ae41519232ba1a68c6a0ff0392064
Druce Chua <89497828+druceC@users.noreply.github.com>
2024-05-10 23:43:24 +0400

    Update ProgressBar.js

--- goodNight/src/screens/ProgressBar.js
+++ goodNight/src/screens/ProgressBar.js
@@ -2,9 +2,11 @@ import React from "react";
 import { View, Animated, StyleSheet } from "react-native";
 import questionsData from "./QuizData";
 
+// Component to display the progress of the quiz
 const ProgressBar = ({ progress }) => {
+    // Extract list of all questions from QuizData file
     const allQuestions = questionsData;
-
+    // Set progress value to update the width of the progress bar
     const progressAnim = progress.interpolate({
         inputRange: [0, allQuestions.length],
         outputRange: ["0%", "100%"],
