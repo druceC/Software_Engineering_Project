08b6024a2015026f5437f91cbb76dcbda824c9c4
Druce Chua <89497828+druceC@users.noreply.github.com>
2024-05-10 14:52:35 +0400

    Update Questions.js

--- goodNight/src/screens/Questions.js
+++ goodNight/src/screens/Questions.js
@@ -1,38 +1,42 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import data from "./QuizData";
 
+// Test question counter display
 const Questions = ({ index, question }) => {
   return (
-    <View style={{}}>
-      {/* Question Counter */}
-      <View
-        style={{
-          flexDirection: "row",
-          alignItems: "flex-end",
-        }}
-      >
-        <Text
-          style={{ color: "#333", fontSize: 15, opacity: 0.6, marginRight: 2 }}
-        >
-          {index + 1}
-        </Text>
-        <Text style={{ color: "#333", fontSize: 13, opacity: 0.6 }}>
-          / {data.length}
-        </Text>
-      </View>
-
-      {/* Question */}
-      <Text
-        style={{
-          color: "#333",
-          fontSize: 18,
-          textAlign: "center",
-        }}
-      >
-        {question}
-      </Text>
-    </View>
+    // Main container
+    React.createElement(View, { style: {} },
+      // Question counter
+      React.createElement(View, 
+        { style: { 
+          flexDirection: "row", 
+          alignItems: "flex-end" 
+        } },
+        // Question index
+        React.createElement(Text, 
+          { style: { 
+            color: "#333", 
+            fontSize: 15, 
+            opacity: 0.6, 
+            marginRight: 2 } }, 
+            index + 1),
+        // Total # of question
+        React.createElement(Text, 
+          { style: { 
+            color: "#333", 
+            fontSize: 13, 
+            opacity: 0.6 } }, 
+            `/ ${data.length}`)
+      ),
+      // Actual question text
+      React.createElement(Text, 
+        { style: { 
+          color: "#333", 
+          fontSize: 18, 
+          textAlign: "center" 
+        } }, question)
+    )
   );
 };
 
